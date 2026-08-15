43f741c0d5736c7a1e5db92da402fda6ba9e04b4
= <=>
2026-08-15 16:52:36 +0700

    Rumus

---
+++ jajar_genjang.js
@@ -0,0 +1,12 @@
+const prompt = require("prompt-sync") ({ sigint: true })
+console.log(" +-=-=-=-=-=-=-=-=-=-=-=-=-=+ ")
+console.log(" | KALKULATOR JAJAR_GENJANG | ")
+console.log(" +-=-=-=-=-=-=-=-=-=-=-=-=-=+ ")
+let a = Number(prompt("Alas           : "))
+let b = Number(prompt("Sisi_miring    : "))
+let t = Number(prompt("Tinggi         : "))
+let Luas = a * t
+let Keliling = 2 * (a + b)
+console.log(`
+Luas        : ${Luas}_cm2
+Keliling    : ${Keliling}_cm2`)
